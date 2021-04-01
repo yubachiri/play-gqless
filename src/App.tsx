8b5d58ba1b0017c784aa6f90258fb1ec077d0e7d
yubachiri <k.i.moso9532@gmail.com>
2021-04-01 15:15:41 +0900

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useQuery } from './gqless'
+import { useQuery } from './gqless';
 
 function App() {
-  const { viewer } = useQuery()
+  const { viewer, $state } = useQuery()
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          {viewer.name}
-        </p>
-        {viewer?.followers({ first: 5 })?.nodes?.map(follower => {
-          return (
-            <p key={follower?.id}>{follower?.name}</p>
-          )
-        })}
+        {$state.isLoading ? <p>loading</p> : (
+          <>
+            <p>{viewer.name}</p>
+            {viewer?.followers({ first: 5 })?.nodes?.map((follower, index) => {
+              return <p key={follower?.id || index}>{follower?.name}</p>
+            })}
+          </>
+        )}
       </header>
     </div>
   );
